2af6653f91471b1addc8884df927aec86e0ef1da
Varshanth2025 <varshanthg2030@gmail.com>
2025-11-04 20:02:41 +0530

    main-landing-page

---
+++ data/tools.js
@@ -0,0 +1,38 @@
+export const tools = [
+  {
+    name: "Next.js",
+    description:
+      "Used to build the full-stack web application with fast rendering, routing, and API routes for backend logic.",
+    image: "/tools-logos/nextjs.png",
+  },
+  {
+    name: "Tailwind CSS",
+    description:
+      "Used for styling and layout, creating a responsive, clean, and modern UI quickly.",
+    image: "/tools-logos/tailwind.png",
+  },
+  {
+    name: "Prisma ORM",
+    description:
+      "Used for managing and querying the PostgreSQL database with type-safe models.",
+    image: "/tools-logos/prismaorm.png",
+  },
+  {
+    name: "PostgreSQL (Neon DB)",
+    description:
+      "Used as the main database to store user data, career details, and AI-generated results securely.",
+    image: "/tools-logos/neon.png",
+  },
+  {
+    name: "Clerk Authentication",
+    description:
+      "Used to handle user authentication and secure access with email and OAuth sign-ins.",
+    image: "/tools-logos/clerk.png",
+  },
+  {
+    name: "Gemini AI (Google AI)",
+    description:
+      "Used to generate personalized career insights, resume improvements, and AI-driven recommendations.",
+    image: "/tools-logos/gemini.png",
+  },
+];
